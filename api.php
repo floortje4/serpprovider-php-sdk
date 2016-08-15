9ec0e24a4912e71a03f170111294acaea0af1d6e
floortje4 <floortje4@gmail.com>
2016-08-15 23:02:22 +0200

    api.php
    SerpProvider PHP API

---
+++ api.php
@@ -0,0 +1,34 @@
+<?php
+
+define("SERPPROVIDER_FETCHMETHOD", "fopen"); //curl or fopen
+define("SERPPROVIDER_KEY", "your-api-key"); //get your api key at serpprovider.com
+define("SERPPROVIDER_ENDPOINT", "http://api.serpprovider.com/"); 
+define("SERPPROVIDER_OUTPUT", "json"); // json or array 
+
+class SerpProvider{
+	public static function query($provider,$country,$language,$device,$keyword){
+		//build url
+		$sUrl = SERPPROVIDER_ENDPOINT.SERPPROVIDER_KEY.'/'.$provider.'/'.$country.'-'.$language.'/'.$device.'/'.urlencode(strtolower($keyword));
+		
+		// fetch json results
+		$json = call_user_func_array('self::'.SERPPROVIDER_FETCHMETHOD,array($sUrl));
+		
+		// return output as json or array
+		return (SERPPROVIDER_OUTPUT == 'json')? $json : json_decode($json,true);
+	}
+
+	private static function curl($sUrl){
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $sUrl);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		$data = curl_exec($curl);
+		curl_close($curl);
+		return $data;
+	}
+
+	private static function fopen($sUrl){
+		return file_get_contents($sUrl);
+
+	}
+}
