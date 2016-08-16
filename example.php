e6ed3bbf440dc83e9d964b3946068394c8da7bab
Floortje <floortje4@gmail.com>
2016-08-16 23:10:10 +0200

    Update example.php

--- example.php
+++ example.php
@@ -1,4 +1,10 @@
 <?php
+
+// include api first
 include('api.php');
 
-echo SerpProvider::query('google','nl','nl','desktop','test');
+// simple example, query google.com in englist for the keyword test
+echo SerpProvider::query('google','en','us','desktop','test');
+
+// advanced example, query google.com in englist for the keyword test, limit to 10 results, then return all urls matching wikipedia
+echo SerpProvider::query('google','en','us','desktop','test',['limit'=>10,'match'=>'wikipedia.com']);
